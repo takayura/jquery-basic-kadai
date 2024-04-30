0d59c5f7785a979482bdb128746be66d49abaa51
鷹羽ゆら <162697800+takayura@users.noreply.github.com>
2024-04-30 11:29:56 +0900

    Add files via upload

---
+++ kadai_006/weather.js
@@ -0,0 +1,21 @@
+// jQueryでheadingクラスを追加して、「今日の天気予報」の文字色を赤にしてください。
+// $(function() {
+//   // id属性がaddの要素がクリックされたら
+//   $('#add').on('click', function(){
+//     // id属性がtargetの要素にclass属性'sample'を追加
+//     $('#target').addClass('sample');
+//   });
+// });
+
+//  (function() {
+//    // HTMLが読み込まれたら
+//    $('window').on('load', function(){
+//      // id属性がtargetの要素にclass属性'heading'を追加
+//      $('#target').addClass('heading');
+//    });
+//  });
+
+$(function() {
+  // id属性がtargetの要素にclass属性'heading'を追加
+  $('#target').addClass('heading');
+});
